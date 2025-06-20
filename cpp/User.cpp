a3ddeb88f2255bba987aa66c12ca2324083fc4c9
aShinde <ananta.shinde@honeywellcom>
2025-06-20 09:50:58 +0530

    20/06/2025

---
+++ cpp/User.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+class User{
+    int id;
+    char name[40];
+    char email[100];
+    char password[20];
+    char address[100];
+
+    public :
+    // User(){
+    //     id = 100;
+    // }
+    // User(char value[]){
+    //     id = 100;
+    //    strcpy(name,value);
+    // }
+    User(int userid,char username[],char useremail[],char userpassword[]){
+        id = userid;
+       strcpy(name,username);
+       strcpy(email,useremail);
+       strcpy(password,userpassword);
+    }
+    void display(){
+        printf("%d, %s",id,name);
+    }
+
+};
+int main(int argc, char const *argv[])
+{
+    User* user = new User(100,"Ananta","demo@gmail","1234566");
+    user->display();
+
+    return 0;
+}
